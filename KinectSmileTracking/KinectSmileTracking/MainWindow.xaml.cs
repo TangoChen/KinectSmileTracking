edc4799546e185457b7b686855ddea50d10af332
TangoChen <tan9ochen@gmail.com>
2013-05-25 17:43:09 +0800

    First committing.

---
+++ KinectSmileTracking/KinectSmileTracking/MainWindow.xaml.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+using Microsoft.Kinect.Toolkit;
+
+/*
+ * Kinect Smile Tracking - Developed by TangoChen
+ * www.TangoChen.com
+ * Twitter.com/TangoChen
+ * Youtube.com/tan9ochen
+ */
+
+namespace KinectSmileTracking
+{
+    using System;
+    using System.Windows;
+    using System.Windows.Data;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using Microsoft.Kinect;
+    using Microsoft.Kinect.Toolkit;
+    using FaceTrackingBasics;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly int Bgr32BytesPerPixel = (PixelFormats.Bgr32.BitsPerPixel + 7) / 8;
+        private readonly KinectSensorChooser sensorChooser = new KinectSensorChooser();
+        private WriteableBitmap colorImageWritableBitmap;
+        private byte[] colorImageData;
+        private ColorImageFormat currentColorImageFormat = ColorImageFormat.Undefined;
+
+        private float modifyValue = 3.3f;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var faceTrackingViewerBinding = new Binding("Kinect") { Source = sensorChooser };
+            faceTrackingViewer.SetBinding(FaceTrackingViewer.KinectProperty, faceTrackingViewerBinding);
+
+            sensorChooser.KinectChanged += SensorChooserOnKinectChanged;
+
+            sensorChooser.Start();
+
+
+        }
+
+        private void SensorChooserOnKinectChanged(object sender, KinectChangedEventArgs kinectChangedEventArgs)
+        {
+            KinectSensor oldSensor = kinectChangedEventArgs.OldSensor;
+            KinectSensor newSensor = kinectChangedEventArgs.NewSensor;
+
+            if (oldSensor != null)
+            {
+                oldSensor.AllFramesReady -= KinectSensorOnAllFramesReady;
+                oldSensor.ColorStream.Disable();
+                oldSensor.DepthStream.Disable();
+                oldSensor.DepthStream.Range = DepthRange.Default;
+                oldSensor.SkeletonStream.Disable();
+                oldSensor.SkeletonStream.EnableTrackingInNearRange = false;
+                oldSensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Default;
+            }
+
+            if (newSensor != null)
+            {
+                try
+                {
+                    newSensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+                    newSensor.DepthStream.Enable(DepthImageFormat.Resolution320x240Fps30);
+                    /*
+                    try
+                    {
+                        // This will throw on non Kinect For Windows devices.
+                        newSensor.DepthStream.Range = DepthRange.Near;
+                        newSensor.SkeletonStream.EnableTrackingInNearRange = true;
+                    }
+                    catch (InvalidOperationException)
+                    {
+                        newSensor.DepthStream.Range = DepthRange.Default;
+                        newSensor.SkeletonStream.EnableTrackingInNearRange = false;
+                    }
+                    */
+                    newSensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Seated;
+                    newSensor.SkeletonStream.Enable();
+                    newSensor.AllFramesReady += KinectSensorOnAllFramesReady;
+                }
+                catch (InvalidOperationException)
+                {
+                    // This exception can be thrown when we are trying to
+                    // enable streams on a device that has gone away.  This
+                    // can occur, say, in app shutdown scenarios when the sensor
+                    // goes away between the time it changed status and the
+                    // time we get the sensor changed notification.
+                    //
+                    // Behavior here is to just eat the exception and assume
+                    // another notification will come along if a sensor
+                    // comes back.
+                }
+            }
+        }
+
+        private void WindowClosed(object sender, EventArgs e)
+        {
+            sensorChooser.Stop();
+            faceTrackingViewer.Dispose();
+        }
+
+        private void KinectSensorOnAllFramesReady(object sender, AllFramesReadyEventArgs allFramesReadyEventArgs)
+        {
+            using (var colorImageFrame = allFramesReadyEventArgs.OpenColorImageFrame())
+            {
+                if (colorImageFrame == null)
+                {
+                    return;
+                }
+
+                // Make a copy of the color frame for displaying.
+                var haveNewFormat = this.currentColorImageFormat != colorImageFrame.Format;
+                if (haveNewFormat)
+                {
+                    this.currentColorImageFormat = colorImageFrame.Format;
+                    this.colorImageData = new byte[colorImageFrame.PixelDataLength];
+                    this.colorImageWritableBitmap = new WriteableBitmap(
+                        colorImageFrame.Width, colorImageFrame.Height, 96, 96, PixelFormats.Bgr32, null);
+                    ColorImage.Source = this.colorImageWritableBitmap;
+                }
+
+                colorImageFrame.CopyPixelDataTo(this.colorImageData);
+                this.colorImageWritableBitmap.WritePixels(
+                    new Int32Rect(0, 0, colorImageFrame.Width, colorImageFrame.Height),
+                    this.colorImageData,
+                    colorImageFrame.Width * Bgr32BytesPerPixel,
+                    0);
+
+                /*
+                double length = Point.Subtract(faceTrackingViewer.ppLeft, faceTrackingViewer.ppRight).Length;
+                tB.Text = length.ToString();
+                if (length > 19)
+                {
+                    elp.Fill = Brushes.Red;
+                }
+                else
+                {
+                    elp.Fill = Brushes.Green;
+                }
+                */
+
+                double mouthWidth = faceTrackingViewer.mouthWidth;
+                double noseWidth = faceTrackingViewer.noseWidth;
+
+                double threshold = noseWidth * modifyValue;
+
+                tBMouthDistance.Text = mouthWidth.ToString();
+                tbThreshold.Text = threshold.ToString();
+
+                if (mouthWidth > threshold)
+                {
+                    elp.Fill = Brushes.Red;
+                }
+                else
+                {
+                    elp.Fill = Brushes.Green;
+                }
+            }
+        }
+
+        private void sldModify_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if(sldModify.IsInitialized)
+            {
+                modifyValue = (float)sldModify.Value;
+                tbModify.Text = modifyValue.ToString();
+            }
+        }
+    }
+}
